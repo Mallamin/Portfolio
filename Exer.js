00106ec6f969f42a92212002402a36b70f9edf66
Mallamin <mallamin@yahoo.com>
2020-02-03 16:35:00 +0100

    changes

---
+++ Exer.js
@@ -0,0 +1,84 @@
+//Operators
+//What are the values of a and x
+let a = 2;
+
+let x = 1 + (a*= 2)
+x=5
+a=4
+ 
+console.log("x: "+x,"A: "+a)
+
+
+
+ // What are the results of the following expressions
+ "" + 1 + 0
+ console.log("" + 1 + 0)
+ 10
+
+ "" - 1 + 0
+ console.log("" - 1 + 0)
+ -1
+ true + false 
+ console.log(true + false)
+ 6 / "3"
+ console.log (6 / "3")
+ 2
+ 4 +5 + "px"
+ console.log(4 + 5 +"px")
+ 9px
+
+ "$" + 4 + 5
+ console.log("$" +4 + 5)
+ $45
+ "4" - 2
+ console.log("4"-2)
+ 2
+ "4px" - 2
+ console.log("4px" - 2)
+ NaN
+ 7 / 0
+ console.log(7 / 0)
+ Infinity
+
+ "  -9 " + 5
+ console.log("  -9 " + 5)
+ undefined
+ "  -9" - 5
+ console.log("  -9 " - 5)
+ -14
+ null+ 1
+ console.log(null+1)
+ 1
+ undefined + 1
+ console.log(undefined +1)
+ NaN
+ " \t \n" - 2
+ console.log(" \t \n" -2)
+ -2
+
+ // Comparison
+ 5>4
+ console.log(5>4)
+ true
+ "apple" > "pineapple"
+ console.log("apple">"pineapple")
+ false
+ undefined==nullconsole.log(undefined==null)
+ true
+ undefined===null
+ console.log(undefined===null)
+ false
+ null== "\n0\n"
+ console.log(null=="\n0\n")
+ false
+ null=== +"\n0\n"
+ console.log(null === + "\n0\n")
+ false
+
+ // conditional operators
+ //will alert be shown? 
+ if ("0") {
+     console.log('Hello');//it will
+     
+     
+ }
