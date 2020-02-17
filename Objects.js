b589de74a35f8bac67df0526d8ff87aa128be4d2
Mallamin <mallamin@yahoo.com>
2020-02-17 17:39:31 +0100

    changes

---
+++ Objects.js
@@ -0,0 +1,64 @@
+// let user = {
+//   name: 'Antony',
+//   isAdmin: true,
+//   Age: 37,
+//   'likes pokemons': true,
+// }
+
+// user.isAdmin = false
+
+// let user = {
+//   name: 'John',
+//   age: 30,
+//   'likes birds': true,
+// }
+// console.log(user.name)
+// console.log(user['likes birds'])
+// delete user['likes birds']
+
+// let user = {}
+// console.log(user)
+// user.name = 'John'
+// console.log(user)
+// user.surname = 'Smith'
+// console.log(user)
+// user.name = 'Pete'
+// console.log(user)
+// delete user.name
+// console.log(user)
+
+// let user = {
+//   name: "John",
+//   surname: "Smith",
+//    name:"Pete",
+//  delete user.name
+
+// };
+
+//
+//console.log(user)
+
+// const user={
+//   name: "Lamin"
+
+// }
+// user.name="Jalloh"
+// console.log(user.name)
+
+//Need to revisit this
+
+let owner = {
+  name: 'John',
+  age: '26',
+  height: '1m',
+}
+
+function isEmpty(objectFromOutside) {
+  for (let property in objectFromOutside) {
+    return false
+  }
+  return true
+}
+
+let result = isEmpty(owner)
+console.log(result)
